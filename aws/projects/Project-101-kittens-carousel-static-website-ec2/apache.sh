6cbceadc7ab7e665405be5d043acd4c552d8bce0
wookee min <wookeemin@wookees-Air.fritz.box>
2022-06-09 21:59:04 +0200

    Updated Carousel101 files

--- aws/projects/Project-101-kittens-carousel-static-website-ec2/apache.sh
+++ aws/projects/Project-101-kittens-carousel-static-website-ec2/apache.sh
@@ -4,6 +4,8 @@ yum update -y
 yum install httpd -y 
 chmod +r /var/www/html
 
+cd /var/www/html 
+
 FOLDER= https://raw.githubusercontent.com/wookee/my-repository/main/Project-101-kittens-carousel-static-website-ec2/static-web #paste from raw index.html in Github
 wget ${FOLDER}/index.html
 wget ${FOLDER}/cat0.jpg
